80ad069600a8c403587a6a23aef68554a8873d53
Tarun868 <156053690+Tarun868@users.noreply.github.com>
2024-01-17 16:11:09 -0600

    Update FizzBuzz.java

--- src/Loops_and_conditions/FizzBuzz.java
+++ src/Loops_and_conditions/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main(String[] args){
         int i;
-        for (i=0;i<=100;i++){
+        for (i=1;i<=100;i++){
             if(i%3==0 && i%5==0){
                 System.out.println("FizzBuzz");
             }
