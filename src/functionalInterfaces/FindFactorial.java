aa247d899c2e84e5d93026d6d25663527931d93d
Tarun868 <156053690+Tarun868@users.noreply.github.com>
2024-02-09 15:26:14 -0600

    added files

---
+++ src/functionalInterfaces/FindFactorial.java
@@ -0,0 +1,16 @@
+package functionalInterfaces;
+
+public class FindFactorial {
+    public static int factorialOfNumber(int num){
+        int fact=1;
+        if(num<0)
+            return -1;
+        else if (num==0||num==1)
+            return 1;
+        else{
+            for(int i=2;i<=num;i++)
+                fact=fact*i;
+        }
+        return fact;
+    }
+}
