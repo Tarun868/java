c4e6b918bd31dbc2a67a64c9d130dbf56c5e997a
Tarun868 <156053690+Tarun868@users.noreply.github.com>
2024-02-09 15:24:00 -0600

    added new files

---
+++ src/functionalInterfaces/RepeatedChar.java
@@ -0,0 +1,16 @@
+package functionalInterfaces;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class RepeatedChar {
+    public String findRepeatedChar(String string){
+        string=string.toLowerCase();
+        char[] charArray=string.toCharArray();
+        HashSet<Character> result=new HashSet<>();
+        for(char c : charArray){
+            if(!result.add(c))
+                result.add(c);
+        }
+        return result.toString();
+    }
+}
